1d2f4c35bd46e65b7395ac585a1413c7f3dcbe8b
Mustafa Zaki Assagaf <mustafa.segf@gmail.com>
2024-06-22 17:24:15 +0700

    feat: add drawing grid

--- main.c
+++ main.c
@@ -1,8 +1,6 @@
-#include <malloc.h>
 #include <conio.h>
 #include <dos.h>
-#include <stdio.h>
-#include <string.h>
+#include <malloc.h>
 
 #define NUM_COLORS 256
 
@@ -26,30 +24,45 @@ byte far *VGA = (byte far *)0xA0000000L;
 #define MAX(x, y) ((x) > (y) ? (x) : (y))
 #define MIN(x, y) ((x) < (y) ? (x) : (y))
 
-void set_mode(byte mode)
-{
+#define GRID_COLS 10
+#define GRID_ROWS 10
+
+#define BLACK 0x00
+#define BLUE 0x01
+#define GREEN 0x02
+#define CYAN 0x03
+#define RED 0x04
+#define MAGENTA 0x05
+#define BROWN 0x06
+#define LIGHT_GRAY 0x07
+#define GRAY 0x08
+#define LIGHT_BLUE 0x09
+#define LIGHT_GREEN 0x0A
+#define LIGHT_CYAN 0x0B
+#define LIGHT_RED 0x0C
+#define LIGHT_MAGENTA 0x0D
+#define YELLOW 0x0E
+#define WHITE 0x0F
+
+void set_mode(byte mode) {
   union REGS regs;
   regs.h.ah = SET_MODE;
   regs.h.al = mode;
   int86(VIDEO_INT, &regs, &regs);
 }
 
-void draw_background()
-{
+void draw_background() {
   int x, y;
 
-  for (y = 0; y < SCREEN_HEIGHT; ++y)
-  {
-    for (x = 0; x < SCREEN_WIDTH; ++x)
-    {
+  for (y = 0; y < SCREEN_HEIGHT; ++y) {
+    for (x = 0; x < SCREEN_WIDTH; ++x) {
       // SETPIX(x, y, y);
-      SETPIX(x, y, 0x0f);
+      SETPIX(x, y, GRAY);
     }
   }
 }
 
-void draw_triangle(int x1, int y1, int x2, int y2, int x3, int y3, byte color)
-{
+void draw_triangle(int x1, int y1, int x2, int y2, int x3, int y3, byte color) {
   long x, y;
   long t1, t2, t3;
   long minx, maxx, miny, maxy;
@@ -59,35 +72,91 @@ void draw_triangle(int x1, int y1, int x2, int y2, int x3, int y3, byte color)
   miny = MIN(y1, MIN(y2, y3));
   maxy = MAX(y1, MAX(y2, y3));
 
-  for (y = miny; y <= maxy; ++y)
-  {
-    for (x = minx; x <= maxx; ++x)
-    {
+  for (y = miny; y <= maxy; ++y) {
+    for (x = minx; x <= maxx; ++x) {
       t1 = (x - x1) * (y2 - y1) - (y - y1) * (x2 - x1);
       t2 = (x - x2) * (y3 - y2) - (y - y2) * (x3 - x2);
       t3 = (x - x3) * (y1 - y3) - (y - y3) * (x1 - x3);
-      if ((t1 >= 0 && t2 >= 0 && t3 >= 0) || (t1 <= 0 && t2 <= 0 && t3 <= 0))
-      {
+      if ((t1 >= 0 && t2 >= 0 && t3 >= 0) || (t1 <= 0 && t2 <= 0 && t3 <= 0)) {
         SETPIX(x, y, color);
       }
     }
   }
 }
 
-draw_square(int x, int y, int size, byte color)
-{
+void draw_square(int x, int y, int size, byte color) {
   int i, j;
-  for (i = x; i < x + size; i++)
-  {
-    for (j = y; j < y + size; j++)
-    {
+  for (i = x; i < x + size; i++) {
+    for (j = y; j < y + size; j++) {
       SETPIX(i, j, color);
     }
   }
 }
 
-int main()
-{
+void draw_line(int x1, int y1, int x2, int y2, byte color) {
+  int x, y;
+  int dx, dy;
+  int dx2, dy2;
+  int ix, iy;
+  int d;
+
+  dx = x2 - x1;
+  dy = y2 - y1;
+
+  dx2 = dx * 2;
+  dy2 = dy * 2;
+
+  ix = (dx > 0) ? 1 : -1;
+  iy = (dy > 0) ? 1 : -1;
+
+  dx = abs(dx);
+  dy = abs(dy);
+
+  if (dx >= dy) {
+    d = dy2 - dx;
+    while (x1 != x2) {
+      SETPIX(x1, y1, color);
+      if (d >= 0) {
+        y1 += iy;
+        d -= dx2;
+      }
+      d += dy2;
+      x1 += ix;
+    }
+  } else {
+    d = dx2 - dy;
+    while (y1 != y2) {
+      SETPIX(x1, y1, color);
+      if (d >= 0) {
+        x1 += ix;
+        d -= dy2;
+      }
+      d += dx2;
+      y1 += iy;
+    }
+  }
+}
+
+void draw_grid() {
+  int i, j;
+  int cell_width = SCREEN_WIDTH / GRID_COLS;
+  int cell_height = SCREEN_HEIGHT / GRID_ROWS;
+
+  for (i = 0; i < GRID_COLS; i++) {
+    for (j = 0; j < GRID_ROWS; j++) {
+      draw_line(i * cell_width, j * cell_height, (i + 1) * cell_width,
+                j * cell_height, WHITE);
+
+      draw_line(i * cell_width, j * cell_height, i * cell_width,
+                (j + 1) * cell_height, WHITE);
+
+    }
+  }
+  draw_line(0, SCREEN_HEIGHT - 1, SCREEN_WIDTH - 1, SCREEN_HEIGHT - 1, WHITE);
+  draw_line(SCREEN_WIDTH - 1, 0, SCREEN_WIDTH - 1, SCREEN_HEIGHT - 1, WHITE);
+}
+
+int main() {
   char kc = 0;
   char s[255];
   byte *pal;
@@ -96,18 +165,15 @@ int main()
 
   draw_background();
 
-  draw_triangle(10, 190,
-                150, 10,
-                310, 190,
-                0x28);
+  draw_grid();
+
+  // draw_triangle(10, 190, 150, 10, 310, 190, 0x28);
 
   // draw_square(10, 10, 100, 0x28);
 
   /* loop until ESC pressed */
-  while (kc != 0x1b)
-  {
-    if (kbhit())
-    {
+  while (kc != 0x1b) {
+    if (kbhit()) {
       kc = getch();
     }
   }
